d05ce2b2d8f8709875d72159905f17ef3bfe99aa
Jim Maciejewski <jim.maciejewski@madisonav.com.au>
2023-11-22 11:12:06 +1000

    Added bits to audio

--- svsi_protocol.lua
+++ svsi_protocol.lua
@@ -15,204 +15,204 @@ local audio_rates = {
 }
 
 local audio_channels = {
-  [0x3c] = "Channels 1 and 2",
-  [0x3d] = "Channels 3 and 4",
-  [0x3e] = "Channels 5 and 6",
-  [0x3f] = "Channels 7 and 8"
+  [0x3c] = "1 and 2",
+  [0x3d] = "3 and 4",
+  [0x3e] = "5 and 6",
+  [0x3f] = "7 and 8"
 }
 
 local speaker_placements = {
   [0] = {
-    [0x3c] = "FR (Front Right) FL (Front Left)",
-    [0x3d] = "- -",
-    [0x3e] = "- -",
-    [0x3f] = "- -"
+    [0x3c] = "1-Front Left 2-Front Right",
+    [0x3d] = "3-None 4-None",
+    [0x3e] = "5-None 6-None",
+    [0x3f] = "7-None 8-None"
     },
   [1] = {
-    [0x3c] = "FR (Front Right) FL (Front Left)",
-    [0x3d] = "- LFE (Low Frequency Effect)",
-    [0x3e] = "- -",
-    [0x3f] = "- -"
+    [0x3c] = "1-Front Left 2-Front Right",
+    [0x3d] = "3-Low Frequency Effect 4-None",
+    [0x3e] = "5-None 6-None",
+    [0x3f] = "7-None 8-None"
   },
   [2] = {
-    [0x3c] = "FR (Front Right) FL (Front Left)",
-    [0x3d] = "FC (Front Centre) -",
-    [0x3e] = "- -",
-    [0x3f] = "- -"
+    [0x3c] = "1-Front Left 2-Front Right",
+    [0x3d] = "3-None 4-Front Centre",
+    [0x3e] = "5-None 6-None",
+    [0x3f] = "7-None 8-None"
   },
   [3] = {
-    [0x3c] = "FR (Front Right) FL (Front Left)",
-    [0x3d] = "FC (Front Centre) LFE (Low Frequency Effect)",
-    [0x3e] = "- -",
-    [0x3f] = "- -"
+    [0x3c] = "1-Front Left 2-Front Right",
+    [0x3d] = "3-Low Frequency Effect 4-Front Centre",
+    [0x3e] = "5-None 6-None",
+    [0x3f] = "7-None 8-None"
   },
   [4] = {
-    [0x3c] = "FR (Front Right) FL (Front Left)",
-    [0x3d] = "- -",
-    [0x3e] = "- RC (Rear Centre)",
-    [0x3f] = "- -"
+    [0x3c] = "1-Front Left 2-Front Right",
+    [0x3d] = "3-None 4-None",
+    [0x3e] = "5-Rear Centre 6-None",
+    [0x3f] = "7-None 8-None"
   },
   [5] = {
-    [0x3c] = "FR (Front Right) FL (Front Left)",
-    [0x3d] = "- LFE (Low Frequency Effect)",
-    [0x3e] = "- RC (Rear Centre)",
-    [0x3f] = "- -"
+    [0x3c] = "1-Front Left 2-Front Right",
+    [0x3d] = "3-Low Frequency Effect 4-None",
+    [0x3e] = "5-Rear Centre 6-None",
+    [0x3f] = "7-None 8-None"
   },
   [6] = {
-    [0x3c] = "FR (Front Right) FL (Front Left)",
-    [0x3d] = "FC (Front Centre) -",
-    [0x3e] = "- RC (Rear Centre)",
-    [0x3f] = "- -"
+    [0x3c] = "1-Front Left 2-Front Right",
+    [0x3d] = "3-None 4-Front Centre",
+    [0x3e] = "5-Rear Centre 6-None",
+    [0x3f] = "7-None 8-None"
   },
   [7] = {
-    [0x3c] = "FR (Front Right) FL (Front Left)",
-    [0x3d] = "FC (Front Centre) LFE (Low Frequency Effect)",
-    [0x3e] = "- RC (Rear Centre)",
-    [0x3f] = "- -"
+    [0x3c] = "1-Front Left 2-Front Right",
+    [0x3d] = "3-Low Frequency Effect 4-Front Centre",
+    [0x3e] = "5-Rear Centre 6-None",
+    [0x3f] = "7-None 8-None"
   },
   [8] = {
-    [0x3c] = "FR (Front Right) FL (Front Left)",
-    [0x3d] = "- -",
-    [0x3e] = "RR (Rear Right) RL (Rear Left)",
-    [0x3f] = "- -"
+    [0x3c] = "1-Front Left 2-Front Right",
+    [0x3d] = "3-None 4-None",
+    [0x3e] = "5-Rear Left 6-Rear Right",
+    [0x3f] = "7-None 8-None"
   },
   [9] = {
-    [0x3c] = "FR (Front Right) FL (Front Left)",
-    [0x3d] = "- LFE (Low Frequency Effect)",
-    [0x3e] = "RR (Rear Right) RL (Rear Left)",
-    [0x3f] = "- -"
+    [0x3c] = "1-Front Left 2-Front Right",
+    [0x3d] = "3-Low Frequency Effect 4-None",
+    [0x3e] = "5-Rear Left 6-Rear Right",
+    [0x3f] = "7-None 8-None"
   },
   [10] = {
-    [0x3c] = "FR (Front Right) FL (Front Left)",
-    [0x3d] = "FC (Front Centre) -",
-    [0x3e] = "RR (Rear Right) RL (Rear Left)",
-    [0x3f] = "- -"
+    [0x3c] = "1-Front Left 2-Front Right",
+    [0x3d] = "3-None 4-Front Centre",
+    [0x3e] = "5-Rear Left 6-Rear Right",
+    [0x3f] = "7-None 8-None"
   },
   [11] = {
-    [0x3c] = "FR (Front Right) FL (Front Left)",
-    [0x3d] = "FC (Front Centre) LFE (Low Frequency Effect)",
-    [0x3e] = "RR (Rear Right) RL (Rear Left)",
-    [0x3f] = "- -"
+    [0x3c] = "1-Front Left 2-Front Right",
+    [0x3d] = "3-Low Frequency Effect 4-Front Centre",
+    [0x3e] = "5-Rear Left 6-Rear Right",
+    [0x3f] = "7-None 8-None"
   },
   [12] = {
-    [0x3c] = "FR (Front Right) FL (Front Left)",
-    [0x3d] = "- -",
-    [0x3e] = "RR (Rear Right) RL (Rear Left)",
-    [0x3f] = "RC (Rear Centre) -"
+    [0x3c] = "1-Front Left 2-Front Right",
+    [0x3d] = "3-None 4-None",
+    [0x3e] = "5-Rear Left 6-Rear Right",
+    [0x3f] = "7-Rear Centre 8-None"
   },
   [13] = {
-    [0x3c] = "FR (Front Right) FL (Front Left)",
-    [0x3d] = "- LFE (Low Frequency Effect)",
-    [0x3e] = "RR (Rear Right) RL (Rear Left)",
-    [0x3f] = "RC (Rear Centre) -"
+    [0x3c] = "1-Front Left 2-Front Right",
+    [0x3d] = "3-Low Frequency Effect 4-None",
+    [0x3e] = "5-Rear Left 6-Rear Right",
+    [0x3f] = "7-Rear Centre 8-None"
   },
   [14] = {
-    [0x3c] = "FR (Front Right) FL (Front Left)",
-    [0x3d] = "FC (Front Centre) -",
-    [0x3e] = "RR (Rear Right) RL (Rear Left)",
-    [0x3f] = "RC (Rear Centre) -"
+    [0x3c] = "1-Front Left 2-Front Right",
+    [0x3d] = "3-None 4-Front Centre",
+    [0x3e] = "5-Rear Left 6-Rear Right",
+    [0x3f] = "7-Rear Centre 8-None"
   },
   [15] = {
-    [0x3c] = "FR (Front Right) FL (Front Left)",
-    [0x3d] = "FC (Front Centre) LFE (Low Frequency Effect)",
-    [0x3e] = "RR (Rear Right) RL (Rear Left)",
-    [0x3f] = "RC (Rear Centre) -"
+    [0x3c] = "1-Front Left 2-Front Right",
+    [0x3d] = "3-Low Frequency Effect 4-Front Centre",
+    [0x3e] = "5-Rear Left 6-Rear Right",
+    [0x3f] = "7-Rear Centre 8-None"
   },
   [16] = {
-    [0x3c] = "FR (Front Right) FL (Front Left)",
-    [0x3d] = "- -",
-    [0x3e] = "RR (Rear Right) RL (Rear Left)",
-    [0x3f] = "RRC (Rear Centre Right) RLC (Rear Centre Left)"
+    [0x3c] = "1-Front Left 2-Front Right",
+    [0x3d] = "3-None 4-None",
+    [0x3e] = "5-Rear Left 6-Rear Right",
+    [0x3f] = "7-Rear Centre Left 8-Rear Centre Right"
   },
   [17] = {
-    [0x3c] = "FR (Front Right) FL (Front Left)",
-    [0x3d] = "- LFE (Low Frequency Effect)",
-    [0x3e] = "RR (Rear Right) RL (Rear Left)",
-    [0x3f] = "RRC (Rear Centre Right) RLC (Rear Centre Left)"
+    [0x3c] = "1-Front Left 2-Front Right",
+    [0x3d] = "3-Low Frequency Effect 4-None",
+    [0x3e] = "5-Rear Left 6-Rear Right",
+    [0x3f] = "7-Rear Centre Left 8-Rear Centre Right"
   },
   [18] = {
-    [0x3c] = "FR (Front Right) FL (Front Left)",
-    [0x3d] = "FC (Front Centre) -",
-    [0x3e] = "RR (Rear Right) RL (Rear Left)",
-    [0x3f] = "RRC (Rear Centre Right) RLC (Rear Centre Left)"
+    [0x3c] = "1-Front Left 2-Front Right",
+    [0x3d] = "3-None 4-Front Centre",
+    [0x3e] = "5-Rear Left 6-Rear Right",
+    [0x3f] = "7-Rear Centre Left 8-Rear Centre Right"
   },
   [19] = {
-    [0x3c] = "FR (Front Right) FL (Front Left)",
-    [0x3d] = "FC (Front Centre) LFE (Low Frequency Effect)",
-    [0x3e] = "RR (Rear Right) RL (Rear Left)",
-    [0x3f] = "RRC (Rear Centre Right) RLC (Rear Centre Left)"
+    [0x3c] = "1-Front Left 2-Front Right",
+    [0x3d] = "3-Low Frequency Effect 4-Front Centre",
+    [0x3e] = "5-Rear Left 6-Rear Right",
+    [0x3f] = "7-Rear Centre Left 8-Rear Centre Right"
   },
   [20] = {
-    [0x3c] = "FR (Front Right) FL (Front Left)",
-    [0x3d] = "- -",
-    [0x3e] = "- -",
-    [0x3f] = "FRC (Front Centre Right) FLC (Front Centre Left)"
+    [0x3c] = "1-Front Left 2-Front Right",
+    [0x3d] = "3-None 4-None",
+    [0x3e] = "5-None 6-None",
+    [0x3f] = "7-Front Centre Left 8-Front Centre Right"
   },
   [21] = {
-    [0x3c] = "FR (Front Right) FL (Front Left)",
-    [0x3d] = "- LFE (Low Frequency Effect)",
-    [0x3e] = "- -",
-    [0x3f] = "FRC (Front Centre Right) FLC (Front Centre Left)"
+    [0x3c] = "1-Front Left 2-Front Right",
+    [0x3d] = "3-Low Frequency Effect 4-None",
+    [0x3e] = "5-None 6-None",
+    [0x3f] = "7-Front Centre Left 8-Front Centre Right"
   },
   [22] = {
-    [0x3c] = "FR (Front Right) FL (Front Left)",
-    [0x3d] = "FC (Front Centre) -",
-    [0x3e] = "- -",
-    [0x3f] = "FRC (Front Centre Right) FLC (Front Centre Left)"
+    [0x3c] = "1-Front Left 2-Front Right",
+    [0x3d] = "3-None 4-Front Centre",
+    [0x3e] = "5-None 6-None",
+    [0x3f] = "7-Front Centre Left 8-Front Centre Right"
   },
   [23] = {
-    [0x3c] = "FR (Front Right) FL (Front Left)",
-    [0x3d] = "FC (Front Centre) LFE (Low Frequency Effect)",
-    [0x3e] = "- -",
-    [0x3f] = "FRC (Front Centre Right) FLC (Front Centre Left)"
+    [0x3c] = "1-Front Left 2-Front Right",
+    [0x3d] = "3-Low Frequency Effect 4-Front Centre",
+    [0x3e] = "5-None 6-None",
+    [0x3f] = "7-Front Centre Left 8-Front Centre Right"
   },
   [24] = {
-    [0x3c] = "FR (Front Right) FL (Front Left)",
-    [0x3d] = "- -",
-    [0x3e] = "- RC (Rear Centre)",
-    [0x3f] = "FRC (Front Centre Right) FLC (Front Centre Left)"
+    [0x3c] = "1-Front Left 2-Front Right",
+    [0x3d] = "3-None 4-None",
+    [0x3e] = "5-Rear Centre 6-None",
+    [0x3f] = "7-Front Centre Left 8-Front Centre Right"
   },
   [25] = {
-    [0x3c] = "FR (Front Right) FL (Front Left)",
-    [0x3d] = "- LFE (Low Frequency Effect)",
-    [0x3e] = "- RC (Rear Centre)",
-    [0x3f] = "FRC (Front Centre Right) FLC (Front Centre Left)"
+    [0x3c] = "1-Front Left 2-Front Right",
+    [0x3d] = "3-Low Frequency Effect 4-None",
+    [0x3e] = "5-Rear Centre 6-None",
+    [0x3f] = "7-Front Centre Left 8-Front Centre Right"
   },
   [26] = {
-    [0x3c] = "FR (Front Right) FL (Front Left)",
-    [0x3d] = "FC (Front Centre) -",
-    [0x3e] = "- RC (Rear Centre)",
-    [0x3f] = "FRC (Front Centre Right) FLC (Front Centre Left)"
+    [0x3c] = "1-Front Left 2-Front Right",
+    [0x3d] = "3-None 4-Front Centre",
+    [0x3e] = "5-Rear Centre 6-None",
+    [0x3f] = "7-Front Centre Left 8-Front Centre Right"
   },
   [27] = {
-    [0x3c] = "FR (Front Right) FL (Front Left)",
-    [0x3d] = "FC (Front Centre) LFE (Low Frequency Effect)",
-    [0x3e] = "- RC (Rear Centre)",
-    [0x3f] = "FRC (Front Centre Right) FLC (Front Centre Left)"
+    [0x3c] = "1-Front Left 2-Front Right",
+    [0x3d] = "3-Low Frequency Effect 4-Front Centre",
+    [0x3e] = "5-Rear Centre 6-None",
+    [0x3f] = "7-Front Centre Left 8-Front Centre Right"
   },
   [28] = {
-    [0x3c] = "FR (Front Right) FL (Front Left)",
-    [0x3d] = "- -",
-    [0x3e] = "RR (Rear Right) RL (Rear Left)",
-    [0x3f] = "FRC (Front Centre Right) FLC (Front Centre Left)"
+    [0x3c] = "1-Front Left 2-Front Right",
+    [0x3d] = "3-None 4-None",
+    [0x3e] = "5-Rear Left 6-Rear Right",
+    [0x3f] = "7-Front Centre Left 8-Front Centre Right"
   },
   [29] = {
-    [0x3c] = "FR (Front Right) FL (Front Left)",
-    [0x3d] = "- LFE (Low Frequency Effect)",
-    [0x3e] = "RR (Rear Right) RL (Rear Left)",
-    [0x3f] = "FRC (Front Centre Right) FLC (Front Centre Left)"
+    [0x3c] = "1-Front Left 2-Front Right",
+    [0x3d] = "3-Low Frequency Effect 4-None",
+    [0x3e] = "5-Rear Left 6-Rear Right",
+    [0x3f] = "7-Front Centre Left 8-Front Centre Right"
   },
   [30] = {
-    [0x3c] = "FR (Front Right) FL (Front Left)",
-    [0x3d] = "FC (Front Centre) -",
-    [0x3e] = "RR (Rear Right) RL (Rear Left)",
-    [0x3f] = "FRC (Front Centre Right) FLC (Front Centre Left)"
+    [0x3c] = "1-Front Left 2-Front Right",
+    [0x3d] = "3-None 4-Front Centre",
+    [0x3e] = "5-Rear Left 6-Rear Right",
+    [0x3f] = "7-Front Centre Left 8-Front Centre Right"
   },
   [31] = {
-    [0x3c] = "FR (Front Right) FL (Front Left)",
-    [0x3d] = "FC (Front Centre) LFE (Low Frequency Effect)",
-    [0x3e] = "RR (Rear Right) RL (Rear Left)",
-    [0x3f] = "FRC (Front Centre Right) FLC (Front Centre Left)"
+    [0x3c] = "1-Front Left 2-Front Right",
+    [0x3d] = "3-Low Frequency Effect 4-Front Centre",
+    [0x3e] = "5-Rear Left 6-Rear Right",
+    [0x3f] = "7-Front Centre Left 8-Front Centre Right"
   }
 }
   
@@ -226,7 +226,7 @@ group = ProtoField.uint8("svsi.group", "Group", base.HEX)
 
 packet_count = ProtoField.uint8("svsi.packet_count", "Packet Count", base.DEC )
 audio_rate = ProtoField.uint8("svsi.audio_rate", "Audio Rate", base.HEX, audio_rates)
-audio_channel = ProtoField.uint8("svsi.audio_channel", "Audio Channel", base.HEX, audio_channels)
+audio_channel = ProtoField.uint8("svsi.audio_channel", "Audio Channels", base.HEX, audio_channels)
 channel_count = ProtoField.string("svsi.channel_count", "Channel Count")
 speaker_placement = ProtoField.string("svsi.speaker_placement", "Speaker Placement")
 
@@ -284,10 +284,12 @@ function svsi_protocol.dissector(buffer, pinfo, tree)
     audio_subtree:add(audio_channel, buffer(1, 1))
     
     --Speaker Placement
+    local speaker_subtree = audio_subtree:add(speaker_placement, buffer(8,1)):set_text("Speaker Mapping: " .. "(0x" .. buffer(8,1) .. ")")
     local placement = buffer(8, 1):bitfield(3, 5)
     local channels = tonumber(buffer:bytes(1,1):tohex(),16)
-    audio_subtree:add(channel_count, buffer(8,1)):set_text("Channel Count: " .. buffer(8, 1):bitfield(0, 3))
-    audio_subtree:add(speaker_placement, buffer(8,1)):set_text("Speaker Mapping " .. speaker_placements[placement][channels])
+    local channel_count_string = buffer(8, 1):bitfield(0, 3)
+    speaker_subtree:add(channel_count, buffer(8,1)):set_text(buffer(8, 1):bitfield(0, 1) .. buffer(8, 1):bitfield(1, 1) .. buffer(8, 1):bitfield(2, 1) .. ". .... = Channel Count: " .. buffer(8, 1):bitfield(0, 3) + 1)
+    speaker_subtree:add(speaker_placement, buffer(8,1)):set_text("..." .. buffer(8, 1):bitfield(3, 1) .. " " .. buffer(8, 1):bitfield(4, 1) .. buffer(8, 1):bitfield(5, 1) .. buffer(8, 1):bitfield(6, 1) .. buffer(8, 1):bitfield(7, 1) .. " = Mapping: " .. speaker_placements[placement][channels])
   end
 
   -- Video Only
